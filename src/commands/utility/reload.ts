c410c02f05fd435bea24a1ca954fbbe997bf04d5
Fabian <fabian.willner@gmail.com>
2021-02-25 13:09:17 +0100

    Created a reload command function

---
+++ src/commands/utility/reload.ts
@@ -0,0 +1,47 @@
+import fs = require("fs");
+
+module.exports = {
+    name: "reload",
+    description: "Reloads a command",
+    args: true,
+    execute(message, args, commands) {
+        const commandName = args[0].toLowerCase();
+        const command =
+            commands.get(commandName) ||
+            commands.find(
+                (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+            );
+
+        if (!command) {
+            return message.channel.send(
+                `There is no command with name or alias \`${commandName}\`, ${message.author}!`
+            );
+        }
+
+        const commandFolders = fs.readdirSync("./src/commands");
+        const folderName = commandFolders.find((folder) =>
+            fs
+                .readdirSync(`./src/commands/${folder}`)
+                .includes(`${commandName}.ts`)
+        );
+
+        const {execSync} = require('child_process');
+        let output = execSync(`tsc ${__dirname}\\..\\${folderName}\\${command.name}.ts`);
+
+
+        delete require.cache[
+            require.resolve(`../${folderName}/${command.name}.js`)
+        ];
+
+        try {
+            const newCommand = require(`../${folderName}/${command.name}.js`);
+            commands.set(newCommand.name, newCommand);
+            message.channel.send(`Command \`${command.name}\` was reloaded!`);
+        } catch (error) {
+            console.error(error);
+            message.channel.send(
+                `There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``
+            );
+        }
+    },
+};
