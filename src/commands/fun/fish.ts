af6ae0d0312d5aaf372e86ccd70b60cb5650123c
Fabian <fabian.willner@gmail.com>
2021-05-06 23:38:54 +0200

    Added some other games

---
+++ src/commands/fun/fish.ts
@@ -0,0 +1,38 @@
+import { argumentWrapper } from "../../interfaces/wrapperObject";
+import * as Discord from "discord.js";
+import { token } from "../../../credentials.json";
+
+module.exports = {
+    name: "fish",
+    aliases: ["fishington", "fishington.io", "angeln"],
+    description: "Fishington.io a game",
+    async execute(message: Discord.Message, context: argumentWrapper) {
+        const voiceChannel: Discord.VoiceChannel = message.member.voice.channel;
+        if (!voiceChannel) {
+            return;
+        }
+        const fetch = require('node-fetch');
+        const response = await fetch(`https://discord.com/api/v8/channels/${voiceChannel}/invites`, {
+            method: "POST",
+            body: JSON.stringify({
+                max_age: 86400,
+                max_uses: 0,
+                target_application_id: "814288819477020702", // fish
+                target_type: 2,
+                temporary: false,
+                validate: null
+            }),
+            headers: {
+                "Authorization": `Bot ${token}`,
+                "Content-Type": "application/json"
+            }
+        })
+        const invite = await response.json()
+
+        if (!invite.code) {
+            message.channel.send("Cannot start Fishington.io, please retry")
+        } else {
+            message.channel.send(`Click on the Link to start fishing Together:\n> https://discord.com/invite/${invite.code}`);
+        }
+    },
+};
---
+++ src/commands/fun/poker.ts
@@ -0,0 +1,37 @@
+import { argumentWrapper } from "../../interfaces/wrapperObject";
+import * as Discord from "discord.js";
+import { token } from "../../../credentials.json";
+
+module.exports = {
+    name: "poker",
+    description: "Play poker",
+    async execute(message: Discord.Message, context: argumentWrapper) {
+        const voiceChannel: Discord.VoiceChannel = message.member.voice.channel;
+        if (!voiceChannel) {
+            return;
+        }
+        const fetch = require('node-fetch');
+        const response = await fetch(`https://discord.com/api/v8/channels/${voiceChannel}/invites`, {
+            method: "POST",
+            body: JSON.stringify({
+                max_age: 86400,
+                max_uses: 0,
+                target_application_id: "755827207812677713", // Poker
+                target_type: 2,
+                temporary: false,
+                validate: null
+            }),
+            headers: {
+                "Authorization": `Bot ${token}`,
+                "Content-Type": "application/json"
+            }
+        })
+        const invite = await response.json()
+
+        if (!invite.code) {
+            message.channel.send("Cannot start Poker, please retry")
+        } else {
+            message.channel.send(`Click on the Link to start playing Poker Together:\n> https://discord.com/invite/${invite.code}`);
+        }
+    },
+};
